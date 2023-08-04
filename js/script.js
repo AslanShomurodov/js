80a63b610f8c6552163b0edc37cbb0a09009eb7a
-Aslam <-shomurodovaslam584@gmail.com>
2023-08-04 16:23:26 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let one  = prompt("son kiriting:")
+// let action  = prompt("amalni kiriting:")
+let two = prompt("son kiriting:")
+
+// console.log(one + two)
+// let onenum = 20
+// let twonum = 20
+
+console.log(+one + +two)
+console.log(one - two)
+console.log(one * two)
+console.log(one / two)
